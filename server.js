1ac300cccb48223eb8bc7e2890dfe55a2ce8bb6c
Tom Logan <tomlogan@umich.edu>
2017-09-27 01:53:32 -0400

    openshift attempt

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-var express = require('express');
-var app = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-app.use('/', express.static(__dirname + '/'));
-app.listen(3000, function() { console.log('listening')});
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function () {
+
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
+
+});
+
+// var express = require('express');
+// var app = express();
+
+// app.use('/', express.static(__dirname + '/'));
+// app.listen(3000, function() { console.log('listening')});
