049c5a66c6a7a3e25f2c82278be3e15f0480d001
Williams <tgw@UMICH.EDU>
2017-10-12 21:00:45 -0400

    tims edits - adding logans new hssa function and checking code. all running and working nicely :)

--- code/calcHSSAscores.py
+++ code/calcHSSAscores.py
@@ -22,77 +22,200 @@ db_fn =  '../query_results/sea_5km.db'
 max_dur = 30*60 # 30 minutes
 dem_col_name = 'pop_over_65'
 
-def main(dem_fn, db_fn, dem_col_name):
-    # create connection to the database
-    logger.info('subsetting the database')
-    db = sqlite3.connect(db_fn)
-    cursor = db.cursor()
-
-    # calculate walk scores and investment for origins
-    calcWalkScoresAndInvestment(cursor, db, max_dur, dem_col_name)
-
-    db.close()
-
-def calcWalkScoresAndInvestment(cursor, db, max_dur, dem_col_name):
+def main(db_fn, dem_col_name, max_dur):
     '''
     calculate the walk score for each origin, and the funding allocated to each origin (investment)
     dem_col_name is the column name in the database of the demographic to be used for the investment calculations
     '''
-    logger.info('calculating walk scores')
-    # get np.DataFrame of orig ids
-    orig_ids = db_fns.getTable(cursor, 'orig', [0, -1], ['orig_id', 'pop_over_65'])
+    logger.info('calculating HSSA scores')
+    db = sqlite3.connect(db_fn)
+    cursor = db.cursor()
+
+    # get pd.DataFrame of orig ids and population of interest
+    c_names = db_fns.getColNames(db, 'orig')
+    dem_c_id = np.where(np.array(c_names) == dem_col_name)[0][0]
+    orig_ids = db_fns.getTable(cursor, 'orig', [0, dem_c_id], ['orig_id', dem_col_name])
 
     # initialize dictionaries: HSSA scores, contracts for each origin, and contract allocation
     scores_dict = {}
     orig_contracts = {}
     contract_per_cap = {}
-    contract_data = db_fns.getTable(cursor, 'contracts', [0, 3], ['ContractNo', 'TotalBudgt'])
+
+    # Get and normalize contract data
+    contract_data = normalizeContractData(cursor)
+    # add this into contract per cap dictionary
     for i in range(contract_data.shape[0]):
-        contract_per_cap[contract_data.ix[i,'ContractNo']] = {'amt' : contract_data.ix[i,'TotalBudgt'], 'ppl' : 0}
+        contract_per_cap[contract_data['ContractNo'].iloc[i]] = {'amt' : contract_data.ix[i,'TotalBudgtOriginal'], 'ppl' : 0}
 
     # Loop through each origin id
     for i in range(orig_ids.shape[0]):
+        orig_id = orig_ids.orig_id.iloc[i]
         if i % 100 == 0:
             print('i = {} / {}'.format(i, orig_ids.shape[0]))
-        # find all services for this orig
-        services_pd = getVendorsForOrig(orig_ids.ix[i, 'orig_id'], cursor).drop_duplicates()
-        # initialize contract list for orig i
-        orig_contracts[orig_ids.ix[i,'orig_id']] = {'contracts' : [], 'pop' : orig_ids.ix[i, dem_col_name]}
-        # loop through the services
-        for j in range(services_pd.shape[0]):
-            # get the duration to this service
-            query_str = '''SELECT walking_time FROM destsubset 
-                WHERE dest_id={} AND orig_id={}'''.format(services_pd.ix[j, 'dest_id'], orig_ids.ix[i, 'orig_id'])
-            tmp = cursor.execute(query_str)            
-            duration = tmp.fetchone()
 
-            # add to data frame
-            services_pd.ix[j, 'walking_time'] = duration[0]
-            # add origin pop to the services funding count
-            contract_per_cap[services_pd.ix[j, 'ContractNo']]['ppl'] += orig_ids.ix[i,'pop_over_65']
-            # add contract id to the origin's contracts
-            orig_contracts[orig_ids.ix[i,'orig_id']]['contracts'].append(services_pd.ix[j, 'ContractNo'])
+        # find all services for this orig
+        services_pd = getVendorsForOrig(orig_id, cursor).drop_duplicates()
+        # initialize contract list for orig i (as well as temporary dictionary)
+        orig_contracts[orig_id] = {'cons' : [], 'pop' : orig_ids[dem_col_name].iloc[i]}
+        orig_cons_temp = {'contracts' : {}, 'pop' : orig_ids[dem_col_name].iloc[i]}
+        # create dict to store the distance to each unique contract id 
+        # - we will just want to store the closest for each
+        for c_no in services_pd.ContractNo:
+            orig_cons_temp['contracts'].update({c_no : {'min_dur' : 99999, 'j' : -1}})
+
+        # add origin pop to the services funding count - i.e. these people are using this contract
+        for con_id in orig_cons_temp['contracts'].keys():
+            contract_per_cap[con_id]['ppl'] += orig_ids[dem_col_name].iloc[i]
+            orig_contracts[orig_id]['cons'].append(con_id) # append to master list
+        # get the minimum distance to each unique contract for this origin
+        
+        closest_services_pd = getMinContractDists(cursor, orig_id, services_pd, orig_cons_temp)
+        
         # CALCULATE WALKING SCORE FOR ORIG
-        score = calcHSSAScore(services_pd, cursor, max_dur)
-        scores_dict[orig_ids.ix[i,'orig_id']] = {'HSSA' : score}
+        score = calcHSSAScore(closest_services_pd, cursor, max_dur)
+        scores_dict[orig_id] = {'HSSA' : score}
  
     # calculate per capita spending for each contract
     contract_per_cap = calcPerCapSpending(contract_data, contract_per_cap)
     # calculate spending per origin (update the scores dictionary with this data)
     scores_dict = calcOrigFunding(orig_contracts, contract_per_cap, scores_dict)
 
-    
-    # normalize the scores
+    # extract the scores
     HSSAs = [val['HSSA'] for val in scores_dict.values()]
     investments = [val['investment'] for val in scores_dict.values()]
     scores_pd = pd.DataFrame({'orig_id' : list(scores_dict.keys()), 'HSSAscore' : HSSAs, 'investment' : investments})
-    scores_pd['HSSAscore'] = int(100 * scores_pd['HSSAscore'].divide(max(scores_pd['HSSAscore'])))
+    
+    scores_pd['HSSAscore'] = 100 * scores_pd['HSSAscore'].divide(max(scores_pd['HSSAscore']))
     logger.info('...normalized the scores')
     
     # add scores to database 
     WriteDB(scores_pd, db, 'investment')
     WriteDB(scores_pd, db, 'HSSAscore')
     db.commit()
+
+
+def normalizeContractData(cursor):
+    '''
+    Fetch the contracts data
+    Normalize the spending amounts for each contract id
+    Adapted from code at:
+    https://github.com/GeoDaCenter/contracts/blob/master/analytics/ScoreModel.py
+    '''
+    contract_data = db_fns.getTable(cursor, 'contracts', [0, 3], ['ContractNo', 'TotalBudgt'])
+    contract_data.set_index('ContractNo', inplace=True)
+
+    # convert to dictionary, adding together the amount of any duplicate contract numbers
+    v_amt_table = {}
+    for site_id, amount in contract_data.iterrows():
+        if site_id in v_amt_table.keys():
+            v_amt_table[site_id] += amount[0]
+        else:
+            v_amt_table[site_id] = amount[0]
+
+    all_contracts = list(v_amt_table.values())
+
+    sd = np.nanstd(all_contracts)
+    mean = np.nanmean(all_contracts)
+    
+    for vend_id, amount in v_amt_table.items():
+        v_amt_table[vend_id] = (amount - mean) / sd
+    
+    min_amt = np.nanmin(list(v_amt_table.values()))
+
+    for vend_id, amount in v_amt_table.items():
+        v_amt_table[vend_id] += abs(min_amt)    
+    
+    # convert to pandas dataframe
+    return_table = pd.DataFrame({'ContractNo' : list(v_amt_table.keys()), 'TotalBudgt' : list(v_amt_table.values()),
+        'TotalBudgtOriginal' : all_contracts})
+
+    return return_table
+
+def getVendorsForOrig(orig_id, cursor):
+    '''
+    get all of the vendors within reach of a given origin point
+    note - doesn't actually get the duration (creates a column with 'None')
+    '''
+    tmp = cursor.execute('''SELECT * FROM contracts 
+        WHERE dest_id IN 
+        (SELECT dest_id FROM destsubset WHERE orig_id={})'''
+        .format(orig_id))
+    services_tuple = tmp.fetchall()
+    # convert to pandas data frame
+    services_list = [x for x in services_tuple]
+    services_pd = pd.DataFrame(services_list, columns=db_fns.getColNames(cursor, 'contracts'))
+    # add column for duration
+    services_pd['walking_time'] = -1 
+    return(services_pd)   
+
+def getMinContractDists(cursor, orig_id, services_pd, orig_cons_temp):
+    '''
+    For a single origin, get the minimum distance to each of the contracts it has access to
+    '''
+    j_to_rm = []
+    # loop through the services
+    for j in range(services_pd.shape[0]):
+        # get the duration to this service
+        query_str = '''SELECT walking_time FROM destsubset 
+            WHERE dest_id={} AND orig_id={}'''.format(services_pd.dest_id.iloc[j], orig_id)
+        tmp = cursor.execute(query_str)            
+        duration = tmp.fetchone()
+
+        # add to data frame - this gives a warning but it works
+        services_pd.walking_time.iloc[j] = duration[0]
+
+        # update the dictionary entry for this contract number - select minimum distance
+        con_id = services_pd.ContractNo.iloc[j]
+
+        prev_min_dur = orig_cons_temp['contracts'][con_id]['min_dur']
+        if duration[0] < prev_min_dur:
+            # save this distance
+            orig_cons_temp['contracts'][con_id]['min_dur'] = duration[0]
+            # remove the old j
+            j_to_rm += [orig_cons_temp['contracts'][con_id]['j']] if prev_min_dur != 99999 else []
+            # add this j to the dictionary
+            orig_cons_temp['contracts'][con_id]['j'] = j
+        else:
+            j_to_rm += [j]
+
+    # remove all unnecessary j's
+    return services_pd.drop(services_pd.index[j_to_rm])
+
+def calcHSSAScore(services, cursor, max_dur):
+    ''' 
+    Calculate the HSSA score for a given origin
+    Note: this code is adapted from Logan Noel's code
+    https://github.com/GeoDaCenter/contracts/blob/master/analytics/ScoreModel.py
+    '''
+    WEIGHTS = [.1, .25, .5, .75, 1]
+    weight_dict = {}
+    score = 0
+    for i in range(services.shape[0]):
+        cat = services.Project.iloc[i]
+        if cat not in weight_dict:
+            weight_dict[cat] = WEIGHTS
+        if len(weight_dict[cat]) > 0:
+            variety_weight = weight_dict[cat].pop()
+        else:
+            variety_weight = 0
+        distance_weight = linearDecayFunction(services.walking_time.iloc[i], max_dur)
+        # calculate score
+        score += variety_weight * distance_weight * services.TotalBudgt.iloc[i]
+    return(score)
+
+def calcPerCapSpending(contract_data, contract_per_cap):
+    '''
+    for each contract, create key for per capita spending and add to dictionary
+    '''
+    for i in range(contract_data.shape[0]):
+        dict_i = contract_per_cap[contract_data.ContractNo.iloc[i]]
+        # calculate per capita spending
+        if dict_i['ppl'] > 0:
+            d_per_cap = {'per_cap' : dict_i['amt'] / dict_i['ppl']}
+        else:
+            d_per_cap = {'per_cap' : 0}     
+        dict_i.update(d_per_cap)
+    return(contract_per_cap)
 
 def calcOrigFunding(orig_contracts, contract_per_cap, scores_dict):
     ''' 
@@ -104,23 +227,13 @@ def calcOrigFunding(orig_contracts, contract_per_cap, scores_dict):
     output_dict = {}
     for orig_id, orig_data in orig_contracts.items():
         orig_spending = 0
-        for contract_id in orig_data['contracts']:
+        
+        for contract_id in orig_data['cons']:
             per_cap_spend = contract_per_cap[contract_id]['per_cap']
             orig_spending += per_cap_spend * orig_data['pop']
+
         scores_dict[orig_id].update({'investment' : orig_spending})
     return(scores_dict)
-
-def calcPerCapSpending(contract_data, contract_per_cap):
-    # for each contract, create key for per capita spending and add to dictionary
-    for i in range(contract_data.shape[0]):
-        dict_i = contract_per_cap[contract_data.ix[i,'ContractNo']]
-        # calculate per capita spending
-        if dict_i['ppl']:
-            d_per_cap = {'per_cap' : dict_i['amt'] / dict_i['ppl']}
-        else:
-            d_per_cap = {'per_cap' : 0}     
-        dict_i.update(d_per_cap)
-    return(contract_per_cap)
 
 def WriteDB(df, db, col_name):
     '''
@@ -141,29 +254,6 @@ def WriteDB(df, db, col_name):
 
     db.commit()
 
-def calcHSSAScore(services, cursor, max_dur):
-    ''' 
-    Calculate the HSSA score for a given origin
-    Note: this code is adapted from Logan Noel's code
-    https://github.com/GeoDaCenter/contracts/blob/master/analytics/ScoreModel.py
-    '''
-    WEIGHTS = [.1, .25, .5, .75, 1]
-    weight_dict = {}
-    score = 0
-    for i in range(services.shape[0]):
-        cat = services.ix[i, 'Project']
-        if cat not in weight_dict:
-            weight_dict[cat] = WEIGHTS
-        if len(weight_dict[cat]) > 0:
-            variety_weight = weight_dict[cat].pop()
-        else:
-            variety_weight = 0
-        distance_weight = linearDecayFunction(services.ix[i, 'walking_time'], max_dur)
-        # calculate score
-        score += variety_weight * distance_weight * services.ix[i,'TotalBudgt']
-    return(score)
-
-
 def linearDecayFunction(time, upper):
     '''
     penalty function for distance
@@ -176,22 +266,5 @@ def linearDecayFunction(time, upper):
     else:
         return (upper - time) / upper
 
-def getVendorsForOrig(orig_id, cursor):
-    '''
-    get all of the vendors within reach of a given origin point
-    note - doesn't actually get the duration (creates a column with 'None')
-    '''
-    tmp = cursor.execute('''SELECT * FROM contracts 
-        WHERE dest_id IN 
-        (SELECT dest_id FROM destsubset WHERE orig_id={})'''
-        .format(orig_id))
-    services_tuple = tmp.fetchall()
-    # convert to pandas data frame
-    services_list = [x for x in services_tuple]
-    services_pd = pd.DataFrame(services_list, columns=db_fns.getColNames(cursor, 'contracts'))
-    # add column for duration
-    services_pd['walking_time'] = None 
-    return(services_pd)   
-
 if __name__ == '__main__':
-    main(dem_fn, db_fn, dem_col_name)
+    main(db_fn, dem_col_name, max_dur)
--- code/workflow.py
+++ code/workflow.py
@@ -22,6 +22,7 @@ import add_demographics
 import calcHSSAscores
 import subset_database
 import duplicateDB
+from shutil import copyfile
 import aggregate_scores
 import dbToGeoJson
 import logging
@@ -32,19 +33,21 @@ logger = logging.getLogger(__name__)
 # input data (required)
 shp_fn = '../data/seattle_contracts/seattle-contracts-geocoded.shp'
 block_fn = '../data/block_data/sea_blocks_wgs84.shp'
-db_fn = '../query_results/sea_5km.db'
+db_master_fn = '../query_results/new_hssa/combined-data_5km_master.db' # this is the input database name
 dem_fn = '../data/demographic/nhgis0002_ds172_2010_block.csv'
 
 # other intermediate inputs and outputs
+db_fn = '../query_results/new_hssa/sea_5km.db' # this database will be generated
+copy_fn = '../query_results/new_hssa/sea_5km_after_hssa.db'
 max_dur = 30*60 # 30 minutes
 dem_field_for_hssa = ('pop_over_65', True)
 demographic_fields = [('pop_female','H76026'),('pop_below_10',True),('pop_color',True), ('pop_total','H76001') ]
-db_api_subset_name = '../query_results/sea_API.db'
+db_api_subset_name = '../query_results/new_hssa/sea_API.db'
 tables_for_api_subset = ['orig', 'contracts', 'destsubset']
 aggregate_levels = ['district', 'neighborhood']
 values_for_aggregate = ['pop_total', 'pop_over_65', 'pop_below_10', 'pop_female', 'pop_color', 'investment']
 scores = ['score0_49', 'score50_69','score70_89','score90_100']
-aggregate_db_fn = '../query_results/sea_boundaries.db'
+aggregate_db_fn = '../query_results/new_hssa/sea_boundaries.db'
 json_outdir = '../data/frontend_plotting/'
 
 
@@ -63,7 +66,8 @@ add_demographics.main(dem_field_for_hssa, dem_fn, db_fn)
 
 # calculate the HSSA scores and funding allocation
 logger.info('calcHSSAscores...')
-calcHSSAscores.main(dem_fn, db_fn, dem_field_for_hssa[0])
+calcHSSAscores.main(db_fn, dem_field_for_hssa[0], max_dur)
+copyfile(db_fn, copy_fn)
 
 # add the rest of the demographics
 for field in demographic_fields:
@@ -78,10 +82,46 @@ logger.info('aggregate_scores...')
 for unit in aggregate_levels:
     aggregate_scores.main(unit, scores, values_for_aggregate, block_fn, db_fn, aggregate_db_fn)
 
+
 # convert to json-esque format
 logger.info('dbToGeoJson...')
 # NOTE: This is not generalizable if adding new geographies
-all_levels = aggregate_levels + ['block']
+all_levels = aggregate_levels #+ ['block']
 for level in all_levels:
-    dbToGeoJson.main(level, json_outdir)
+    dbToGeoJson.main(level, json_outdir, aggregate_db_fn)
 
+
+
+
+import shp2db
+import add_demographics
+import calcHSSAscores
+import subset_database
+import duplicateDB
+import aggregate_scores
+import dbToGeoJson
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#### file names and parameters ####
+# input data (required)
+shp_fn = '../data/seattle_contracts/seattle-contracts-geocoded.shp'
+block_fn = '../data/block_data/sea_blocks_wgs84.shp'
+db_master_fn = '../query_results/new_hssa/combined-data_5km_master.db' # this is the input database name
+dem_fn = '../data/demographic/nhgis0002_ds172_2010_block.csv'
+
+# other intermediate inputs and outputs
+db_fn = '../query_results/new_hssa/sea_5km.db' # this database will be generated
+max_dur = 30*60 # 30 minutes
+dem_field_for_hssa = ('pop_over_65', True)
+demographic_fields = [('pop_female','H76026'),('pop_below_10',True),('pop_color',True), ('pop_total','H76001') ]
+db_api_subset_name = '../query_results/new_hssa/sea_API.db'
+tables_for_api_subset = ['orig', 'contracts', 'destsubset']
+aggregate_levels = ['district', 'neighborhood']
+values_for_aggregate = ['pop_total', 'pop_over_65', 'pop_below_10', 'pop_female', 'pop_color', 'investment']
+scores = ['score0_49', 'score50_69','score70_89','score90_100']
+aggregate_db_fn = '../query_results/new_hssa/sea_boundaries.db'
+json_outdir = '../data/frontend_plotting/'
+
+aggregate_scores.main(aggregate_levels[0], scores, values_for_aggregate, block_fn, db_fn, aggregate_db_fn)
